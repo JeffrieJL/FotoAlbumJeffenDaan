646d280ad3421069cf208871f924934ff5c4b214
Jeffrie Van Oijen <jeffrievanoijen@outlook.com>
2016-07-07 09:34:31 +0200

    begin gemaakt

---
+++ app/albumcomponent/album.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'album',
+    templateUrl: './app/albumcomponent/album.html',
+    styleUrls: ['./app/albumcomponent/album.css']
+})
+
+export class AlbumComponent{
+
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AlbumComponent } from './albumcomponent/album.component';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app/startPage.html'
+    templateUrl: './app/startPage.html',
+    styleUrls: ['./app/startPage.css'],
+    directives: [AlbumComponent]
 })
 
 export class AppComponent{
